644fe926563b51b585a6514f27a3e389f5cd71c3
Steve Bour <bourpower@gmail.com>
2021-12-26 17:25:03 -0500

    added logic for  coloring, refined timeblocs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,43 @@
 var date = moment();
 $("#currentDay").text(date.format("dddd, MMM Do YYYY"));
 
-if (moment().format('h') < window.hour) {
-    window.timeBlock.attr('background-color', 'red');
-} else if (moment().format('h') > window.hour) {
-    window.timeBlock.attr('background-color', 'green');
-} else {
-    window.timeBlock.attr('background-color', 'grey');
+var appts = {
+    "9": [],
+    "10": [],
+    "11": [],
+    "12": [],
+    "13": [],
+    "14": [],
+    "15": [],
+    "16": [],
+    "17": []
 };
+
+var setAppts = function () {
+    localStorage.setItem("appts", JSON.stringify(appts));
+}
+
+var getAppts = function () {
+    JSON.parse(localStorage.getItem("appts"));
+}
+
+var timeCheck = function () {
+    var time = moment().hour();
+    $(".appt-info").each(function () {
+        var apptHour = parseInt($(this).attr("id"));
+        if (time > apptHour) {
+            $(this).addClass("past");
+        } else if (time < apptHour) {
+            $(this).addClass("future");
+        } else {
+            $(this).addClass("present");
+        }
+    })
+};
+
+var timeUpdate = 3600000 - date.milliseconds();
+setTimeout(function () {
+    setInterval(timeCheck, 3600000)
+}, timeUpdate);
+
+timeCheck();
